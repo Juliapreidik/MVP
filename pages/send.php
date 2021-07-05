0c1ef7161921764752d4081ef086f4a04719fa4a
JuliaPreidikman <juliapreidik@hotmail.com>
2021-07-04 23:04:39 -0300

    agregue el index.html a mi carpeta

---
+++ pages/send.php
@@ -0,0 +1,10 @@
+<?php
+    $destino = "mateo.varelal@gmail.com";
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+    $contenido = "Name: " . $name . "\nE-Mail: " . $email . "\nPhone Number: " . $phone . "\nMessage: " . $message ;
+    mail($destino, "Get in Touch", $contenido);
+    header("Location: thankyou.html");   
+?>    
